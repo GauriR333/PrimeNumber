a009ea0f857628c51dcb9db459cc6eb5b63bc91f
Gauri Ragade <gauriragade98@gmail.com>
2023-01-10 23:42:52 +0530

    Prime number program

---
+++ PrimeNumber/src/com/power/PrimeNumber/PrimeNumber.java
@@ -0,0 +1,24 @@
+package com.power.PrimeNumber;
+
+public class PrimeNumber {
+	public static void main(String args[]) {
+		//long n = 53;
+		long n = 365241862;
+		boolean isPrime = true;
+
+		long currentTimeMillis = System.currentTimeMillis();
+		
+		for (int j = 2; j < n/2; j++) {
+			if (n % j == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime == true) {
+			System.out.println("This is prime Number");
+		} else {
+			System.out.println("This is not prime  Number");
+		}
+		System.out.println( System.currentTimeMillis()-currentTimeMillis);
+	}
+}
